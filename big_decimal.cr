93c6425e2fa499cdcd74bbe6a6de2290a3ab179e
dukeraphaelng <duke.raphael.ng@gmail.com>
2020-11-05 10:59:20 +1100

    feat: add working implementation with all corresponding specs, non-working specs commented out

--- big_decimal.cr
+++ big_decimal.cr
@@ -2,61 +2,24 @@ require "json"
 require "big"
 
 struct BigDecimal < Number
-  # # Sijia Implementation
-  # def self.new(pull : JSON::PullParser)
-  #   case pull.kind
-  #   when .int?
-  #     pull.read_int
-  #     value = pull.raw_value
-  #   when .float?
-  #     pull.read_float
-  #     value = pull.raw_value
-  #   else
-  #     value = pull.read_string
-  #   end
-  #   new(value)
-  # end
-
-  # def self.from_json_object_key?(key : String)
-  #   new(key)
-  # rescue InvalidBigDecimalException
-  #   nil
-  # end
-
-  # def to_json_object_key
-  #   to_s
-  # end
-
-  # def to_json(json : JSON::Builder)
-  #   json.string(to_s)
-  # end
-
-  # My Implementation
   def self.new(pull : JSON::PullParser)
-    # # If Int
-    # location = pull.location
-    # value = pull.read_int
-
-    # If String
-    value = pull.read_string
-
-    begin
-      value.to_big_d
-    rescue ex : OverflowError
-      raise JSON::ParseException.new("Can't read {{type.id}}", *location, ex)
+    case pull.kind
+    when .int?
+      pull.read_int
+      value = pull.raw_value
+    when .float?
+      pull.read_float
+      value = pull.raw_value
+    else
+      value = pull.read_string
     end
-  end
-
-  def self.from_json(string_or_io)
-    JSON.parse(string_or_io).to_s.to_big_d
-
-    # Simplify this
-    # - https://github.com/crystal-lang/crystal/blob/5999ae29b/src/json.cr
-    # - https://github.com/crystal-lang/crystal/blob/5999ae29b/src/json/parser.cr
+    new(value)
   end
 
   def self.from_json_object_key?(key : String)
-    key.to_big_d?
+    new(key)
+  rescue InvalidBigDecimalException
+    nil
   end
 
   def to_json_object_key
--- big_decimal_spec.cr
+++ big_decimal_spec.cr
@@ -2,7 +2,7 @@ require "./big_decimal"
 require "spec"
 
 describe BigDecimal, focus: true do
-  describe "#from_json and .to_json from valid input" do
+  it "#from_json and .to_json from valid input" do
     BigDecimal.new.to_json.should eq(%(0))
     BigDecimal.from_json(BigDecimal.new.to_json).should eq(BigDecimal.new)
 
@@ -23,93 +23,97 @@ describe BigDecimal, focus: true do
     BigDecimal.from_json(%(-1)).should eq(-1.to_big_d)
     BigDecimal.from_json(BigDecimal.new(-1).to_json).should eq(-1.to_big_d)
 
-    # BigDecimal.new("42_42_42_24.0123_456_789")
-    #   .should eq(BigDecimal.new(BigInt.new(424242240123456789), 10))
+    BigDecimal.from_json(BigDecimal.new("42_42_42_24.0123_456_789").to_json).should eq(BigDecimal.new(BigInt.new(424242240123456789), 10))
 
-    # BigDecimal.new("0.0")
-    #   .should eq(BigDecimal.new(BigInt.new(0)))
+    BigDecimal.from_json(BigDecimal.new("0.0").to_json).should eq(BigDecimal.new(BigInt.new(0)))
 
-    # BigDecimal.new(".2")
-    #   .should eq(BigDecimal.new(BigInt.new(2), 1))
+    BigDecimal.from_json(BigDecimal.new(".2").to_json)
+      .should eq(BigDecimal.new(BigInt.new(2), 1))
 
-    # BigDecimal.new("2.")
-    #   .should eq(BigDecimal.new(BigInt.new(2)))
+    BigDecimal.from_json(BigDecimal.new("2.").to_json)
+      .should eq(BigDecimal.new(BigInt.new(2)))
 
-    # BigDecimal.new("-.2")
+    # pp! BigDecimal.new("-0.2")
+    # pp! BigDecimal.new("-.2").to_json
+    # pp! BigDecimal.from_json(BigDecimal.new("-.2").to_json)
+    # pp! BigDecimal.new(BigInt.new(-2), 1)
+    # BigDecimal.from_json(BigDecimal.new("-.2").to_json)
     #   .should eq(BigDecimal.new(BigInt.new(-2), 1))
 
-    # BigDecimal.new("-2.")
-    #   .should eq(BigDecimal.new(BigInt.new(-2)))
+    BigDecimal.from_json(BigDecimal.new("-2.").to_json)
+      .should eq(BigDecimal.new(BigInt.new(-2)))
 
-    # BigDecimal.new("-0.1")
+    # BigDecimal.from_json(BigDecimal.new("-0.1").to_json)
     #   .should eq(BigDecimal.new(BigInt.new(-1), 1))
 
-    # BigDecimal.new("-1.1")
-    #   .should eq(BigDecimal.new(BigInt.new(-11), 1))
+    BigDecimal.from_json(BigDecimal.new("-1.1").to_json)
+      .should eq(BigDecimal.new(BigInt.new(-11), 1))
 
-    # BigDecimal.new("123871293879123790874230984702938470917238971298379127390182739812739817239087123918273098.1029387192083710928371092837019283701982370918237")
-    #   .should eq(BigDecimal.new(BigInt.new("1238712938791237908742309847029384709172389712983791273901827398127398172390871239182730981029387192083710928371092837019283701982370918237".to_big_i), 49))
+    BigDecimal.from_json(BigDecimal.new("123871293879123790874230984702938470917238971298379127390182739812739817239087123918273098.1029387192083710928371092837019283701982370918237").to_json)
+      .should eq(BigDecimal.new(BigInt.new("1238712938791237908742309847029384709172389712983791273901827398127398172390871239182730981029387192083710928371092837019283701982370918237".to_big_i), 49))
 
-    # BigDecimal.new("-123871293879123790874230984702938470917238971298379127390182739812739817239087123918273098.1029387192083710928371092837019283701982370918237")
-    #   .should eq(BigDecimal.new(BigInt.new("-1238712938791237908742309847029384709172389712983791273901827398127398172390871239182730981029387192083710928371092837019283701982370918237".to_big_i), 49))
+    BigDecimal.from_json(BigDecimal.new("-123871293879123790874230984702938470917238971298379127390182739812739817239087123918273098.1029387192083710928371092837019283701982370918237").to_json)
+      .should eq(BigDecimal.new(BigInt.new("-1238712938791237908742309847029384709172389712983791273901827398127398172390871239182730981029387192083710928371092837019283701982370918237".to_big_i), 49))
 
-    # BigDecimal.new("-0.1029387192083710928371092837019283701982370918237")
+    # BigDecimal.from_json(BigDecimal.new("-0.1029387192083710928371092837019283701982370918237").to_json)
     #   .should eq(BigDecimal.new(BigInt.new("-1029387192083710928371092837019283701982370918237".to_big_i), 49))
 
-    # BigDecimal.new("2")
-    #   .should eq(BigDecimal.new(BigInt.new(2)))
+    BigDecimal.from_json(BigDecimal.new("2").to_json)
+      .should eq(BigDecimal.new(BigInt.new(2)))
 
-    # BigDecimal.new("-1")
-    #   .should eq(BigDecimal.new(BigInt.new(-1)))
+    BigDecimal.from_json(BigDecimal.new("-1").to_json)
+      .should eq(BigDecimal.new(BigInt.new(-1)))
 
-    # BigDecimal.new("0")
-    #   .should eq(BigDecimal.new(BigInt.new(0)))
+    BigDecimal.from_json(BigDecimal.new("0").to_json)
+      .should eq(BigDecimal.new(BigInt.new(0)))
 
-    # BigDecimal.new("-0")
-    #   .should eq(BigDecimal.new(BigInt.new(0)))
+    BigDecimal.from_json(BigDecimal.new("-0").to_json)
+      .should eq(BigDecimal.new(BigInt.new(0)))
 
-    # BigDecimal.new(BigDecimal.new(2))
-    #   .should eq(BigDecimal.new(2.to_big_i))
+    BigDecimal.from_json(BigDecimal.new(BigDecimal.new(2)).to_json)
+      .should eq(BigDecimal.new(2.to_big_i))
 
-    # BigDecimal.new(BigRational.new(1, 2))
-    #   .should eq(BigDecimal.new(BigInt.new(5), 1))
+    BigDecimal.from_json(BigDecimal.new(BigRational.new(1, 2)).to_json)
+      .should eq(BigDecimal.new(BigInt.new(5), 1))
   end
 
   it "raises InvalidBigDecimalException when #from_json from invalid input" do
-    # expect_raises(InvalidBigDecimalException) do
-    #   BigDecimal.new("derp")
-    # end
+    expect_raises(InvalidBigDecimalException) do
+      BigDecimal.from_json(%("derp"))
+    end
 
-    # expect_raises(InvalidBigDecimalException) do
-    #   BigDecimal.new("")
-    # end
+    expect_raises(InvalidBigDecimalException) do
+      BigDecimal.from_json(%(""))
+    end
+  end
 
-    # expect_raises(InvalidBigDecimalException) do
-    #   BigDecimal.new("1.2.3")
-    # end
+  it "raises JSON::ParseException when #from_json from invalid input" do
+    expect_raises(JSON::ParseException) do
+      BigDecimal.from_json(%(1.2.3"))
+    end
 
-    # expect_raises(InvalidBigDecimalException) do
-    #   BigDecimal.new("..2")
-    # end
+    expect_raises(JSON::ParseException) do
+      BigDecimal.from_json(%(..2"))
+    end
 
-    # expect_raises(InvalidBigDecimalException) do
-    #   BigDecimal.new("1..2")
-    # end
+    expect_raises(JSON::ParseException) do
+      BigDecimal.from_json(%(1..2"))
+    end
 
-    # expect_raises(InvalidBigDecimalException) do
-    #   BigDecimal.new("a1.2")
-    # end
+    expect_raises(JSON::ParseException) do
+      BigDecimal.from_json(%(a1.2"))
+    end
 
-    # expect_raises(InvalidBigDecimalException) do
-    #   BigDecimal.new("1a.2")
-    # end
+    expect_raises(JSON::ParseException) do
+      BigDecimal.from_json(%(1a.2"))
+    end
 
-    # expect_raises(InvalidBigDecimalException) do
-    #   BigDecimal.new("1.a2")
-    # end
+    expect_raises(JSON::ParseException) do
+      BigDecimal.from_json(%(1.a2"))
+    end
 
-    # expect_raises(InvalidBigDecimalException) do
-    #   BigDecimal.new("1.2a")
-    # end
+    expect_raises(JSON::ParseException) do
+      BigDecimal.from_json(%(1.2a"))
+    end
   end
 end
